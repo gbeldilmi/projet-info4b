f795f0b5b7b998823005eb3215bcffea1863badc
ClementDblt <clementdblt.contact@gmail.com>
2022-03-04 17:45:10 +0100

    File implementation

--- corewar/tests/Test1.java
+++ corewar/tests/Test1.java
@@ -1,6 +1,18 @@
 package corewar.tests;
+
+import java.util.ArrayList;
+import corewar.utils.File;
+
 public class Test1 {
-  public static void main(String[] args) {
-    //
-  }
+    public static void main(String[] args) {
+        test_file_read("text.txt");
+    }
+
+    public static void test_file_read(String path) {
+        ArrayList<String> array = File.read(path);
+        int i;
+
+        for (i = 0; i < array.size(); i++)
+            System.out.println(array.get(i));
+    }
 }
--- corewar/utils/File.java
+++ corewar/utils/File.java
@@ -1,4 +1,37 @@
 package corewar.utils;
+
+import java.util.ArrayList;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class File {
-  //
+	public static ArrayList<String> read(String path) {
+		ArrayList<String> array = new ArrayList<>();
+		String str;
+		int c;
+
+		str = "";
+		try (FileInputStream in = new FileInputStream(path)) {
+			while ((c = in.read()) != -1) {
+				str = str + (char)c;
+				if (c == '\r' || c == '\n') {
+					array.add(str.substring(0, str.length() - 1));
+					str = "";
+					if (c == '\r')
+						in.read(); // skip du '\n' sur windaube
+				}
+			}
+			array.add(str);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return array;
+	}
+
+	public static void write(String path, ArrayList<String> content) {
+
+	}
+
+	// delete
+	// create
 }
