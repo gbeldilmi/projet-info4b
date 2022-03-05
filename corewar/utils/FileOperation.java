a8fd82017077e3387ad76db500ee56646a4739e9
ClementDblt <clementdblt.contact@gmail.com>
2022-03-05 12:38:17 +0100

    FileOperation.java v1.0

--- corewar/utils/FileOperation.java
+++ corewar/utils/FileOperation.java
@@ -3,8 +3,10 @@ package corewar.utils;
 import java.util.ArrayList;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.File;
 
-public class File {
+public class FileOperation {
+	//	Retourne une ArrayList<String> correspondant à chaque lignes du fichier
 	public static ArrayList<String> read(String path) {
 		ArrayList<String> array = new ArrayList<>();
 		String str;
@@ -18,7 +20,7 @@ public class File {
 					array.add(str.substring(0, str.length() - 1));
 					str = "";
 					if (c == '\r')
-						in.read(); // skip du '\n' sur windaube
+						in.read(); // skip du '\n' sur Windows
 				}
 			}
 			array.add(str);
@@ -28,6 +30,7 @@ public class File {
 		return array;
 	}
 
+	//	Reçoit une ArrayList<String> "content" correspondant à chaque lignes du fichier à écrire
 	public static void write(String path, ArrayList<String> content) {
 		int i;
 		int j;
@@ -44,6 +47,25 @@ public class File {
 		}
 	}
 
-	// delete
-	// create
+	//	Création d'un fichier
+	public static void create(String path) {
+		File file = new File(path);
+
+		try {
+			file.createNewFile();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	//	Suppression d'un fichier
+	public static void delete(String path) {
+		File file = new File(path);
+
+		try {
+			file.delete();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
